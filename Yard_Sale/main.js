e3d531722b84594c1accf623913a1fc1203f0c6b
rivaldo <leo_1998@live.cl>
2023-02-12 19:21:29 -0300

    union menu mobile-desktop terminado

--- Yard_Sale/main.js
+++ Yard_Sale/main.js
@@ -1,7 +1,13 @@
 const menuDesk = document.querySelector('.navbar-email')
 const menuchange = document.querySelector('.desktop-menu')
+const m_bar_menu = document.querySelector('.menu')
+const m_menu = document.querySelector('.mobile-menu')
 menuDesk.addEventListener('click',change)
+m_bar_menu.addEventListener('click',change_mobile)
 
 function change(){
     menuchange.classList.toggle('inactive')
+}
+function change_mobile(){
+    m_menu.classList.toggle('inactive')
 }

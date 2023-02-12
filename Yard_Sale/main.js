5ff4de4bd47fd103810417ad32713113c4db4e5b
rivaldo <leo_1998@live.cl>
2023-02-12 20:28:56 -0300

    union 3 elementos

--- Yard_Sale/main.js
+++ Yard_Sale/main.js
@@ -1,14 +1,36 @@
+
 const menuDesk = document.querySelector('.navbar-email')
 const menuchange = document.querySelector('.desktop-menu')
+
 const m_bar_menu = document.querySelector('.menu')
 const m_menu = document.querySelector('.mobile-menu')
 
-menuDesk.addEventListener('click',change)
-m_bar_menu.addEventListener('click',change_mobile)
+const icon_shop = document.querySelector('.navbar-shopping-cart')
+const change_shop = document.querySelector('.product-detail')
 
-function change(){
+const lista = [menuchange,m_menu,change_shop]
+
+
+function close(variable){
+    for (i=0 ; i < lista.length ; i++){
+        if (!lista[i].classList.contains('inactive') && lista[i] != variable){
+            lista[i].classList.toggle('inactive')
+        }
+    }
+}
+menuDesk.addEventListener('click',change_desktop)
+m_bar_menu.addEventListener('click',change_mobile)
+icon_shop.addEventListener('click',Shop_interactive)
+
+function change_desktop(){
+    close(menuchange)
     menuchange.classList.toggle('inactive')
 }
 function change_mobile(){
+    close(m_menu)
     m_menu.classList.toggle('inactive')
+}
+function Shop_interactive(){
+    close(change_shop)
+    change_shop.classList.toggle('inactive')
 }
